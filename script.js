399c9cd9ece1d2189ee9176fcb7ba8dcf8149d32
Diago Dryson <cortezdiago@gmail.com>
2022-06-29 11:49:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ var you;
 var yourScore = 0;
 var opponent;
 var opponentScore = 0;
+
+var choices = ["rock", "paper", "scissors"];
+
+window.onload = function() {
+    for (let i = 0; i < 3; i++) {
+    // img id="rock"
+    let choice = document.createElement("img");
+    choice.id = choices[i];
+    }
+   }
