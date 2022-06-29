33cf0b0cc6e8924b3929412855458ab8a537182f
Diago Dryson <cortezdiago@gmail.com>
2022-06-29 12:18:21 +0200

    mise à jour

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ var choices = ["rock", "paper", "scissors"];
 
 window.onload = function() {
     for (let i = 0; i < 3; i++) {
-    // <img id="rock" src="rock.png">
-    let choice = document.createElement("img");
-    choice.id = choices[i];
-    choice.src = choices[i] + ".png";
-    document.getElementById("choices").append(choice);
-    }
-   }
+        // <img id="rock" src="rock.png">
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice);
+        }
+      }
+
+function selectChoice() {
+    you = this.id;
+    document.getElementById("your-choice").src = you + ".png";
+
+    //random for oppponent
+    opponent = choices[Math.floor(Math.random() * 3)]; //0- .999999 * 3 = 0-2.99999
+    document.getElementById("opponent-choice").src = opponent + ".png";
+}
