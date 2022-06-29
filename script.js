6b36da13e3b249d2540e2974bf6d9759d9aa621a
Diago Dryson <cortezdiago@gmail.com>
2022-06-29 15:00:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ window.onload = function() {
         choice.src = choices[i] + ".png";
         choice.addEventListener("click", selectChoice);
         document.getElementById("choices").append(choice);
-        }
-      }
+    }
+}
 
 function selectChoice() {
     you = this.id;
@@ -24,37 +24,37 @@ function selectChoice() {
     opponent = choices[Math.floor(Math.random() * 3)]; //0- .999999 * 3 = 0-2.99999
     document.getElementById("opponent-choice").src = opponent + ".png";
 
-//check for winner
-if (you == opponent) {
-    yourScore += 1;
-    opponentScore += 1;
-}
-else {
-    if (you == "rock") {
-        if (opponent == "scissors") {
-            yourScore += 1;
+    //check for winner
+    if (you == opponent) {
+        yourScore += 1;
+        opponentScore += 1;
+    }
+    else {
+        if (you == "rock") {
+            if (opponent == "scissors") {
+                yourScore += 1;
+            }
+            else if (opponent == "paper") {
+                opponentScore += 1;
+            }
         }
-        else if (opponent == "paper") {
-            opponentScore += 1;
+        else if (you == "scissors") {
+            if (opponent == "paper") {
+                yourScore += 1;
+            }
+            else if (opponent == "rock") {
+                opponentScore += 1;
+            }
+        }
+        else if (you == "paper") {
+            if (opponent == "rock") {
+                yourScore += 1;
+            }
+            else if (opponent == "scissors") {
+                opponentScore += 1;
+            }
         }
     }
-    else if (you == "scissors") {
-        if (opponent == "paper") {
-            yourScore += 1;
-        }
-        else if (opponent == "rock") {
-            opponentScore += 1;
-        }
-    }
-    else if (you == "paper") {
-        if (opponent == "rock") {
-            yourScore += 1;
-        }
-        else if (opponent == "scissors") {
-            opponentScore += 1;
-        }
-    }
-}
 
     document.getElementById("your-score").innerText = yourScore;
     document.getElementById("opponent-score").innerText = opponentScore;
