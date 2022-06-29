371c0070b96ef1e5fcbc579203184de4f59d5465
Diago Dryson <cortezdiago@gmail.com>
2022-06-29 13:09:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,4 +23,23 @@ function selectChoice() {
     //random for oppponent
     opponent = choices[Math.floor(Math.random() * 3)]; //0- .999999 * 3 = 0-2.99999
     document.getElementById("opponent-choice").src = opponent + ".png";
+
+//check for winner
+if (you == opponent) {
+    yourScore += 1;
+    opponentScore += 1;
+}
+else {
+    if (you == "rock") {
+        if (opponent == "scissors") {
+            yourScore += 1;
+        }
+        else if (opponent == "paper") {
+            opponentScore += 1;
+        }
+    }
+    else if (you == "scissors") {
+        if (opponent == "paper") {
+            yourScore += 1;
+        }
 }
